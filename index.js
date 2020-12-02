bbd097fe7f6baaeaa5f0d17c906338f617b90516
NickVeliki <nickveliki@gmail.com>
2020-12-02 14:10:49 +0100

    promisify getDefinition & quota

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const fs = require("fs");
 const pathreq = require("path");
 const crypto = require("crypto");
+const {totalmem} = require("os");
 let _basePath;
-let _secdatpath
+let _secdatpath;
+let _memoryQuota
 let toWrite = false;
 const db = {}
-const setup = (rel, secdatpath, writeinterval=60)=>new Promise((res, rej)=>{
+const del = {};
+const QuotaReached = ()=>_memoryQuota<=1?process.memoryUsage().rss/totalmem()>_memoryQuota:process.memoryUsage().rss>_memoryQuota*Math.pow(2, 20);
+const setup = (rel, secdatpath, writeinterval=60, memoryQuota=0.2)=>new Promise((res, rej)=>{
     _secdatpath=secdatpath;
-    _basePath=pathreq.join(pathreq.resolve("./"), rel)                
+    _basePath=pathreq.join(pathreq.resolve("./"), rel)
+    _memoryQuota=memoryQuota;                
     fs.access(secdatpath, fs.constants.F_O|fs.constants.R_OK, (err)=>{
         if(err){
             const key= crypto.randomBytes(16)
@@ -68,8 +73,20 @@ const getDB = ()=>new Promise((res, rej)=>{
                     fs.readFile(path, (err, data)=>{
                         if(!err){
                             const piv = iv.includes(",")?Buffer.from(iv.split(",").map((item)=>parseInt(item))):Buffer.from(iv, "base64");
-                            const table = JSON.parse(crypto.createDecipheriv("aes-128-gcm", key, piv).update(data).toString())
-                            db[path.replace(_basePath+pathreq.sep, "").replace(".jdf", "").replace(/\\/g, "/").split("#")[0]]={iv: piv, table}
+                            const {indexKey, Versions} = JSON.parse(crypto.createDecipheriv("aes-128-gcm", key, piv).update(data).toString());
+                            let table = {indexKey}
+                            let quotad;
+                            if(!QuotaReached()){
+                                table["Versions"] = Versions;
+                            }else{
+                                quotad=true;
+                                table["Versions"]=Versions.map((item)=>{
+                                    const rs = {};
+                                    rs[indexKey] = item[indexKey]
+                                    return rs;
+                                });
+                            }
+                            db[path.replace(_basePath+pathreq.sep, "").replace(".jdf", "").replace(/\\/g, "/").split("#")[0]]={iv: piv, table, quotad}
                         }else{
                             console.log(err)
                         }
@@ -84,6 +101,7 @@ const getDB = ()=>new Promise((res, rej)=>{
     })
 })
 const writeDB = ()=>new Promise((res)=>{
+    console.log(process.memoryUsage().rss/Math.pow(2, 20), process.memoryUsage().heapUsed/Math.pow(2, 20), QuotaReached())
     if(toWrite){
         getSec().then(({key, iv})=>{
             const Definitions = []
@@ -96,15 +114,54 @@ const writeDB = ()=>new Promise((res)=>{
                     if(!fs.existsSync(pathreq.join(_basePath, ...item.split("/").splice(0, item.split("/").length-1)))){
                         fs.mkdirSync(pathreq.join(_basePath, ...item.split("/").splice(0, item.split("/").length-1)), {recursive:true})
                     }
-                    fs.writeFile(p+".jdf", crypto.createCipheriv("aes-128-gcm", key, db[item].iv).update(JSON.stringify(db[item].table)), {mode:0o600}, (err)=>{
+                    fs.readFile(p+".jdf", (err, data)=>{
+                        const table = {indexKey: db[item].table.indexKey, Versions: []}
                         if(err){
                             fs.writeFileSync(pathreq.join(_basePath, "error.log"), JSON.stringify({time: new Date().toUTCString(), err}), {flag:"a"})
+                        }else{
+                            const searchKey = db[item].table.indexKey;
+                            const fromFile = JSON.parse(crypto.createDecipheriv("aes-128-gcm", key, db[item].iv).update(data)).Versions.map((it)=>{
+                                const searchValue=it[searchKey];
+                                const {i, before} = searchArray(searchKey, searchValue, db[item].table.Versions)
+                                if(before!=undefined){
+                                    return it
+                                }else{
+                                    return updateObject(it, db[item].table.Versions.splice(i, 1))
+                                }
+                            })
+                            table.Versions=[...fromFile, ...db[item].table.Versions].filter((it)=>{
+                                if(del[item]){
+                                    const {i, before} = searchArray(searchKey, it[searchKey], del[item]);
+                                    if(before==undefined){
+                                        del[item].splice(i, 1);
+                                        return false
+                                    }
+                                }
+                                return true
+                            }).sort((a, b)=>a[searchKey]<b[searchKey]?-1:1);
+                            if(QuotaReached()){
+                                db[item].table.Versions=table.Versions.map((item)=>{
+                                    const res = {};
+                                    res[searchKey]=item[searchKey]
+                                    return res
+                                })
+                                db[item].quotad=true;
+                            }else{
+                                db[item].table.Versions=table.Versions
+                                db[item].quotad=undefined
+                            }
                         }
-                        finished++
-                        if(finished==keys.length+1){
-                            res()
-                        }
+                        fs.writeFile(p+".jdf", crypto.createCipheriv("aes-128-gcm", key, db[item].iv).update(JSON.stringify(table)), {mode:0o600}, (err)=>{
+                            if(err){
+                                fs.writeFileSync(pathreq.join(_basePath, "error.log"), JSON.stringify({time: new Date().toUTCString(), err}), {flag:"a"})
+                            }
+                            finished++
+                            if(finished==keys.length+1){
+                                res()
+                            }
+                        })
                     })
+                    
                 })
                 fs.writeFile(pathreq.join(_basePath, "definitions.jdf"), crypto.createCipheriv("aes-128-gcm", key, iv).update(JSON.stringify({Definitions})), (err)=>{
                     if(err){
@@ -141,7 +198,7 @@ const searchArray = (searchkey, searchvalue, array)=>{
     }
     return {before: true, i: 0}
 }
-const getDefinition = (definition)=>{
+const getDefinitionSync = (definition)=>{
     if(typeof(definition)=="string"&&db[definition]){
         return db[definition].table;
     }else if (definition.path&&db[definition.path]&&definition[db[definition.path].table.indexKey]!=undefined){
@@ -151,6 +208,44 @@ const getDefinition = (definition)=>{
         return undefined;
     }
 }
+const getDefinition = (definition)=>new Promise((res, rej)=>{
+    const sync = getDefinitionSync(definition)
+    if(sync){
+        if(sync.indexKey){
+            if(db[definition].quotad){
+                console.log(definition +" is under quota")
+                getSec().then(({key})=>{
+                    fs.readFile(pathreq.join(_basePath, definition+".jdf"), (err, data)=>{
+                        if(err){
+                            res(sync)
+                        }else{
+                            res(JSON.parse(crypto.createDecipheriv("aes-128-gcm", key, db[definition].iv).update(data).toString()))
+                        }
+                    })
+                })
+            }else{
+                res(sync)
+            }
+        }else{
+            if(Object.keys(sync).length==1){
+                getDefinition(definition.path).then(({Versions})=>{
+                    const {i, before} = searchArray(Object.keys(sync)[0], sync[Object.keys(sync)[0]], Versions);
+                    const ibefore = searchArray(Object.keys(sync)[0], sync[Object.keys(sync)[0]], db[definition.path].table.Versions);
+                    if(before==undefined){
+                        db[definition.path].table.Versions.splice(ibefore.i, 1, Versions[i])
+                        res(Versions[i])
+                    }else{
+                        rej({error: 404})
+                    }
+                }, rej)
+            }else{
+                res(sync)
+            }
+        }
+    }else{
+        rej({error: 404, message:"not found"})
+    }
+})
 const updateObject = function(){
     const res = {};
     Array.from(arguments).forEach((argument)=>{
@@ -166,12 +261,12 @@ const writeDefinition = (definition)=>{
     }else{
         if(definition.path){
             if(db[definition.path]){
-                if(definition[db[definition.path].table.indexKey]){
+                if(definition[db[definition.path].table.indexKey]!=undefined){
                     const {i, before } = searchArray(db[definition.path].table.indexKey, definition[db[definition.path].table.indexKey], db[definition.path].table.Versions);
                     db[definition.path].table.Versions.splice(before==undefined?i:before?i:i+1, before==undefined?1:0, updateObject(before==undefined?db[definition.path].table.Versions[i]:{}, definition, {path: undefined, indexKey: undefined}))
                     toWrite=true;
                 }
-            }else if(definition.indexKey&&definition[definition.indexKey]){
+            }else if(definition.indexKey&&definition[definition.indexKey]!=undefined){
                 db[definition.path]={iv:crypto.randomBytes(16), table:{indexKey: definition.indexKey, Versions: [updateObject(definition, {path: undefined, indexKey: undefined})]}}
                 toWrite=true;
             }
@@ -184,25 +279,30 @@ const deleteDefinition = (definition)=>{
     }else{
         if(definition.path&&db[definition.path]&&definition[db[definition.path].table.indexKey]){
             const {i, before} = searchArray(db[definition.path].table.indexKey, definition[db[definition.path].table.indexKey], db[definition.path].table.Versions);
+            const delpar = searchArray(db[definition.path].table.indexKey, definition[db[definition.path].table.indexKey], del[definition.path])
             if(before==undefined){
-                db[definition.path].table.Versions.splice(i, 1);
+                const delitem = db[definition.path].table.Versions.splice(i, 1);
+                const tdel = {};
+                tdel[db[definition.path].table.indexKey] = delitem[db[definition.path].table.indexKey];
+                del[definition.path].splice(delpar.before?delpar.i:delpar.i+1, 0, tdel)
                 toWrite=true
             }
         }
     }
 }
-const getDefinitionProperty = (definition, property)=>{
+const getDefinitionProperty = (definition, property)=>new Promise((res, rej)=>{
     if(typeof(definition)=="string"&&typeof(property)!="number"&&parseInt(property)==NaN){
-        throw new Error("can only get numbered properties from arrays")
+        rej({error: 409, message:"string definition must come with number property"})
     }else{
-        const Version = getDefinition(definition);
-        if(Version.Versions){
-            return Version.Versions[parseInt(property)]
-        }else if (Version){
-            return Version[property];
-        }
+        getDefinition(definition).then((Version)=>{
+            if(Version.Versions){
+                res(Version.Versions[parseInt(property)])
+            }else if (Version){
+                res(Version[property]);
+            }
+        }, rej) 
     }
-}
+})
 const importTables = (path, secDatPath)=>new Promise((res, rej)=>{
     fs.stat(path, (err, stat)=>{
         if(err){
