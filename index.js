6c3b6ccf15bfe047281eb3eff01cf506ff7cb621
nickveliki <53113228+nickveliki@users.noreply.github.com>
2021-03-07 14:21:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -297,7 +297,8 @@ const deleteDefinition = (definition)=>{
                     toWrite=true
                 }    
             }else if(before==undefined){
-                del[definition.path] = [db.definition.path].table.Versions.splice(i, 1)   
+                del[definition.path] = [db.definition.path].table.Versions.splice(i, 1);
+                toWrite=true
             }
             
         }
